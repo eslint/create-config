fbd1b03743b47a9e516471d8e5696059e4c71129
Brandon Mills <mills.brandont@gmail.com>
2022-08-22 00:54:03 -0400

    chore: Remove unused createCustomTeardown test helper
    This also allows us to remove the `fs-teardown` dependency.

--- tests/_utils/index.js
+++ tests/_utils/index.js
@@ -8,36 +8,11 @@
 //-----------------------------------------------------------------------------
 
 import { defineInMemoryFs } from "./in-memory-fs.js";
-import fsTeardown from "fs-teardown";
-
-const { createTeardown, addFile } = fsTeardown;
-
-//-----------------------------------------------------------------------------
-// Helpers
-//-----------------------------------------------------------------------------
-
-/**
- * Creates a new filesystem volume at the given location with the given files.
- * @param {Object} desc A description of the filesystem volume to create.
- * @param {string} desc.cwd The current working directory ESLint is using.
- * @param {Object} desc.files A map of filename to file contents to create.
- * @returns {Teardown} An object with prepare(), cleanup(), and getPath()
- *      methods.
- */
-function createCustomTeardown({ cwd, files }) {
-    const { prepare, cleanup, getPath } = createTeardown(
-        cwd,
-        ...Object.keys(files).map(filename => addFile(filename, files[filename]))
-    );
-
-    return { prepare, cleanup, getPath };
-}
 
 //-----------------------------------------------------------------------------
 // Exports
 //-----------------------------------------------------------------------------
 
 export {
-    defineInMemoryFs,
-    createCustomTeardown
+    defineInMemoryFs
 };
