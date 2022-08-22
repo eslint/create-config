32a89485d1e255e611ede0e7082598beb7fb94d4
Brandon Mills <mills.brandont@gmail.com>
2022-08-22 00:53:07 -0400

    chore: Remove unused unIndent test helper

--- tests/_utils/index.js
+++ tests/_utils/index.js
@@ -17,23 +17,6 @@ const { createTeardown, addFile } = fsTeardown;
 //-----------------------------------------------------------------------------
 
 /**
- * Prevents leading spaces in a multiline template literal from appearing in the resulting string
- * @param {string[]} strings The strings in the template literal
- * @param {any[]} values The interpolation values in the template literal.
- * @returns {string} The template literal, with spaces removed from all lines
- */
-function unIndent(strings, ...values) {
-    const text = strings
-        .map((s, i) => (i === 0 ? s : values[i - 1] + s))
-        .join("");
-    const lines = text.replace(/^\n/u, "").replace(/\n\s*$/u, "").split("\n");
-    const lineIndents = lines.filter(line => line.trim()).map(line => line.match(/ */u)[0].length);
-    const minLineIndent = Math.min(...lineIndents);
-
-    return lines.map(line => line.slice(minLineIndent)).join("\n");
-}
-
-/**
  * Creates a new filesystem volume at the given location with the given files.
  * @param {Object} desc A description of the filesystem volume to create.
  * @param {string} desc.cwd The current working directory ESLint is using.
@@ -55,7 +38,6 @@ function createCustomTeardown({ cwd, files }) {
 //-----------------------------------------------------------------------------
 
 export {
-    unIndent,
     defineInMemoryFs,
     createCustomTeardown
 };
