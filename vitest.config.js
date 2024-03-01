d8d81d581184204a1df91741a04709a8f6544d96
唯然 <weiran.zsd@outlook.com>
2024-03-01 16:19:39 +0800

    chore: c8 -> v8

---
+++ vitest.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        coverage: {
+            include: ["lib/**/*"],
+            reporter: ["text", "html"]
+        }
+    }
+});
