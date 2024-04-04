54ac1f2bb213e60c9b387c01cc674cb03b1aafed
唯然 <weiran.zsd@outlook.com>
2024-04-04 17:22:34 +0800

    feat!: support flat config (#81)
    * feat!: support flat config
    
    * fix: do not support multi configs
    
    it's still needed in some rare cases, but need a few more refactor
    
    * fix: helpercontent
    
    * fix: review suggestions
    
    * fix: rm unused dev deps
    
    * fix: rm unused dev deps
    
    * fix: add missing @eslint/eslintrc
    
    * chore: clean fixtures
    
    * fix: styleguide tests
    
    * feat: allow styleguide to be string (type = flat)
    
    * fix: sourceType: script
    
    * fix: indent
    
    * fix: use `[].concat()` only for shared configs
    
    * chore: c8 -> v8
    
    * refactor: do not throw in constructor
    
    * fix: rm deps debug
    
    * fix: shortname for scoped packages
    
    * chore: put env in .npmrc
    
    * fix: change cwd to the `package.json` located dir
    
    * fix: review suggestions
    
    * fix: generage config for sub-dir
    
    * Update lib/config-generator.js
    
    Co-authored-by: Francesco Trotta <github@fasttime.org>
    
    * chore: add tests for sub dir
    
    * Update lib/config-generator.js
    
    Co-authored-by: Francesco Trotta <github@fasttime.org>
    
    * Update lib/config-generator.js
    
    Co-authored-by: Francesco Trotta <github@fasttime.org>
    
    * fix: module => moduleType
    
    * fix: only apply sourceType to js files
    
    * fix: problem => problems
    
    * fix: eslint bin
    
    * fix: update snapshots
    
    * chore: refactor export content
    
    Signed-off-by: 唯然 <weiran.zsd@outlook.com>
    
    * fix: review suggestions
    
    * Update README.md
    
    * Update lib/config-generator.js
    
    Co-authored-by: Francesco Trotta <github@fasttime.org>
    
    * Update README.md
    
    Co-authored-by: Francesco Trotta <github@fasttime.org>
    
    * chore: update snapshots
    
    * feat: offical vue supports
    
    * fix: revert re-ordering
    
    * fix: allow skipping installing deps
    
    * Update lib/config-generator.js
    
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>
    
    * chore: update snapshots
    
    ---------
    
    Signed-off-by: 唯然 <weiran.zsd@outlook.com>
    Co-authored-by: Francesco Trotta <github@fasttime.org>
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>

--- bin/create-config.js
+++ bin/create-config.js
@@ -1,10 +1,39 @@
 #!/usr/bin/env node
 
 /**
- * @fileoverview Main CLI that is run via the eslint command.
- * @author Nicholas C. Zakas
+ * @fileoverview Main CLI that is run via the `npm init @eslint/config` command.
+ * @author 唯然<weiran.zsd@outlook.com>
  */
 
-/* eslint no-console:off -- CLI */
-import { initializeConfig } from "../lib/init/config-initializer.js";
-initializeConfig();
+import { ConfigGenerator } from "../lib/config-generator.js";
+import { findPackageJson } from "../lib/utils/npm-utils.js";
+import process from "process";
+
+
+const cwd = process.cwd();
+const packageJsonPath = findPackageJson(cwd);
+
+if (packageJsonPath === null) {
+    throw new Error("A package.json file is necessary to initialize ESLint. Run `npm init` to create a package.json file and try again.");
+}
+
+const argv = process.argv;
+const sharedConfigIndex = process.argv.indexOf("--config");
+
+if (sharedConfigIndex === -1) {
+    const generator = new ConfigGenerator({ cwd, packageJsonPath });
+
+    await generator.prompt();
+    generator.calc();
+    await generator.output();
+} else {
+
+    // passed "--config"
+    const packageName = argv[sharedConfigIndex + 1];
+    const type = argv.includes("--eslintrc") ? "eslintrc" : "flat";
+    const answers = { purpose: "style", moduleType: "module", styleguide: { packageName, type } };
+    const generator = new ConfigGenerator({ cwd, packageJsonPath, answers });
+
+    generator.calc();
+    await generator.output();
+}
---
+++ tests/utils/npm-utils.spec.js
@@ -0,0 +1,227 @@
+/**
+ * @fileoverview Tests for npm-utils.
+ * @author Ian VanSchooten, 唯然<weiran.zsd@outlook.com>
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+import spawn from "cross-spawn";
+import sinon from "sinon";
+import {
+    installSyncSaveDev,
+    fetchPeerDependencies,
+    checkDeps,
+    checkDevDeps
+} from "../../lib/utils/npm-utils.js";
+import { defineInMemoryFs } from "../_utils/in-memory-fs.js";
+import esmock from "esmock";
+import { assert, describe, afterEach, it } from "vitest";
+
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+/**
+ * Import `npm-utils` with the in-memory file system.
+ * @param {Object} files The file definitions.
+ * @returns {Object} `npm-utils`.
+ */
+async function requireNpmUtilsWithInMemoryFileSystem(files) {
+    const fs = defineInMemoryFs({ files });
+
+    return await esmock("../../lib/utils/npm-utils.js", { fs });
+}
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+describe("npmUtils", () => {
+    afterEach(() => {
+        sinon.verifyAndRestore();
+    });
+
+    describe("checkDevDeps()", () => {
+        let installStatus = checkDevDeps(["debug", "eslint", "notarealpackage", "jshint"]);
+
+        it("should not find a direct dependency of the project", () => {
+            assert.isFalse(installStatus.debug);
+        });
+
+        it("should find a dev dependency of the project", () => {
+            assert.isTrue(installStatus.eslint);
+        });
+
+        it("should not find non-dependencies", () => {
+            assert.isFalse(installStatus.notarealpackage);
+        });
+
+        it("should not find nested dependencies", () => {
+            assert.isFalse(installStatus.jshint);
+        });
+
+        it("should return false for a single, non-existent package", () => {
+            installStatus = checkDevDeps(["notarealpackage"]);
+            assert.isFalse(installStatus.notarealpackage);
+        });
+
+        it("should handle missing devDependencies key", async () => {
+            const { checkDevDeps: stubcheckDevDeps } = await requireNpmUtilsWithInMemoryFileSystem({
+                "package.json": JSON.stringify({ private: true, dependencies: {} })
+            });
+
+            // Should not throw.
+            stubcheckDevDeps(["some-package"]);
+        });
+
+        it("should throw with message when parsing invalid package.json", async () => {
+            const { checkDevDeps: stubcheckDevDeps } = await requireNpmUtilsWithInMemoryFileSystem({
+                "package.json": '{ "not: "valid json" }'
+            });
+
+            assert.throws(() => {
+                stubcheckDevDeps(["some-package"]);
+            }, /JSON/u);
+        });
+    });
+
+    describe("checkDeps()", () => {
+        let installStatus = checkDeps(["enquirer", "mocha", "notarealpackage", "jshint"]);
+
+        it("should find a direct dependency of the project", () => {
+            assert.isTrue(installStatus.enquirer);
+        });
+
+        it("should not find a dev dependency of the project", () => {
+            assert.isFalse(installStatus.mocha);
+        });
+
+        it("should not find non-dependencies", () => {
+            assert.isFalse(installStatus.notarealpackage);
+        });
+
+        it("should not find nested dependencies", () => {
+            assert.isFalse(installStatus.jshint);
+        });
+
+        it("should return false for a single, non-existent package", () => {
+            installStatus = checkDeps(["notarealpackage"]);
+            assert.isFalse(installStatus.notarealpackage);
+        });
+
+        it("should throw if no package.json can be found", () => {
+            assert.throws(() => {
+                installStatus = checkDeps(["notarealpackage"], "/fakepath");
+            }, "Could not find a package.json file");
+        });
+
+        it("should handle missing dependencies key", async () => {
+            const { checkDeps: stubbedcheckDeps } = await requireNpmUtilsWithInMemoryFileSystem({
+                "package.json": JSON.stringify({ private: true, devDependencies: {} })
+            });
+
+            // Should not throw.
+            stubbedcheckDeps(["some-package"]);
+        });
+
+        it("should throw with message when parsing invalid package.json", async () => {
+            const { checkDeps: stubbedcheckDeps } = await requireNpmUtilsWithInMemoryFileSystem({
+                "package.json": '{ "not: "valid json" }'
+            });
+
+            assert.throws(() => {
+                stubbedcheckDeps(["some-package"]);
+            }, /JSON/u);
+        });
+    });
+
+    describe("checkPackageJson()", () => {
+        it("should return true if package.json exists", async () => {
+            const { checkPackageJson: stubbedcheckPackageJson } = await requireNpmUtilsWithInMemoryFileSystem({
+                "package.json": '{ "file": "contents" }'
+            });
+
+            assert.strictEqual(stubbedcheckPackageJson(), true);
+        });
+
+        it("should return false if package.json does not exist", async () => {
+            const { checkPackageJson: stubbedcheckPackageJson } = await requireNpmUtilsWithInMemoryFileSystem({});
+
+            assert.strictEqual(stubbedcheckPackageJson(), false);
+        });
+    });
+
+    describe("installSyncSaveDev()", () => {
+        it("should invoke npm to install a single desired package", () => {
+            const stub = sinon.stub(spawn, "sync").returns({ stdout: "" });
+
+            installSyncSaveDev("desired-package", "npm");
+            assert(stub.calledOnce);
+            assert.strictEqual(stub.firstCall.args[0], "npm");
+            assert.deepStrictEqual(stub.firstCall.args[1], ["install", "-D", "desired-package"]);
+            stub.restore();
+        });
+
+        it("should invoke yarn to install a single desired package", () => {
+            const stub = sinon.stub(spawn, "sync").returns({ stdout: "" });
+
+            installSyncSaveDev("desired-package", "yarn");
+            assert(stub.calledOnce);
+            assert.strictEqual(stub.firstCall.args[0], "yarn");
+            assert.deepStrictEqual(stub.firstCall.args[1], ["add", "-D", "desired-package"]);
+            stub.restore();
+        });
+
+
+        it("should accept an array of packages to install", () => {
+            const stub = sinon.stub(spawn, "sync").returns({ stdout: "" });
+
+            installSyncSaveDev(["first-package", "second-package"], "npm");
+            assert(stub.calledOnce);
+            assert.strictEqual(stub.firstCall.args[0], "npm");
+            assert.deepStrictEqual(stub.firstCall.args[1], ["install", "-D", "first-package", "second-package"]);
+            stub.restore();
+        });
+
+        it("should log an error message if npm throws ENOENT error", async () => {
+            const logErrorStub = sinon.spy();
+            const npmUtilsStub = sinon.stub(spawn, "sync").returns({ error: { code: "ENOENT" } });
+
+            const { installSyncSaveDev: stubinstallSyncSaveDev } = await esmock("../../lib/utils/npm-utils.js", {
+                "../../lib/utils/logging.js": {
+                    error: logErrorStub
+                }
+            });
+
+            stubinstallSyncSaveDev("some-package");
+
+            assert(logErrorStub.calledOnce);
+
+            npmUtilsStub.restore();
+        });
+    });
+
+    describe("fetchPeerDependencies()", () => {
+        it("should execute 'npm show --json <packageName> peerDependencies' command", () => {
+            const stub = sinon.stub(spawn, "sync").returns({ stdout: "" });
+
+            fetchPeerDependencies("desired-package");
+            assert(stub.calledOnce);
+            assert.strictEqual(stub.firstCall.args[0], "npm");
+            assert.deepStrictEqual(stub.firstCall.args[1], ["show", "--json", "desired-package", "peerDependencies"]);
+            stub.restore();
+        });
+
+        it("should return null if npm throws ENOENT error", () => {
+            const stub = sinon.stub(spawn, "sync").returns({ error: { code: "ENOENT" } });
+
+            const peerDependencies = fetchPeerDependencies("desired-package");
+
+            assert.isNull(peerDependencies);
+
+            stub.restore();
+        });
+    });
+});
