cba9611983ae243d216a7d08042bb39436d2e572
Wataru Nishimura <wataru.nishimura@socialchangelab.jp>
2024-04-27 16:12:47 +0900

    feat: 🎸 pnpm and yarn-legacy supoort

--- tests/workspace-support.spec.js
+++ tests/workspace-support.spec.js
@@ -14,17 +14,25 @@ const __filename = fileURLToPath(import.meta.url); // eslint-disable-line no-und
 
 describe("pnpm workspace install packages at root", () => {
     const pnpmWithWorkspaceDir = join(__filename, "../fixtures/pnpm-workspace-project");
+    const yarnLegacyWithWorkspaceDir = join(__filename, "../fixtures/yarn-legacy-workspace-project");
 
     afterEach(() => {
         sinon.verifyAndRestore();
     });
 
+    /**
+     * pnpm recognizes whether workspace is enabled by `pnpm-workspace.yaml`.
+     * This test case tests function to find `pnpm-workspace.yaml`.
+     */
     it("find pnpm-workspace.yaml", () => {
         const pnpmWorkspaceYaml = findPnpmWorkspaceYaml(pnpmWithWorkspaceDir);
 
         expect(pnpmWorkspaceYaml).toBeTruthy();
     });
 
+    /**
+     * at project root, `pnpm add` needs to be applied "-w" option.
+     */
     it("should invoke pnpm with workspace option to install a single desired packages", async () => {
         const stub = sinon.stub(spawn, "sync").returns({ stdout: 0 });
 
@@ -34,4 +42,14 @@ describe("pnpm workspace install packages at root", () => {
         assert.deepStrictEqual(stub.firstCall.args[1], ["add", "-D", "-w", "desired-package"]);
         stub.restore();
     });
+
+    it("should invoke yarn legacy with workspace option to install a single desired packages", async () => {
+        const stub = sinon.stub(spawn, "sync").returns({ stdout: 0 });
+
+        installSyncSaveDev("desired-package", yarnLegacyWithWorkspaceDir, "yarn");
+        assert(stub.calledOnce);
+        assert.strictEqual(stub.firstCall.args[0], "yarn");
+        assert.deepStrictEqual(stub.firstCall.args[1], ["add", "-D", "-W", "desired-package"]);
+    });
+
 });
