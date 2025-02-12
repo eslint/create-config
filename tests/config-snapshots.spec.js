2a75349e35d5c4e83168ad83cbc401c1d6c0eaf1
Milos Djermanovic <milos.djermanovic@gmail.com>
2025-02-12 11:03:41 +0100

    feat: avoid empty config warning (#153)
    * feat: avoid empty config warning
    
    * env is always an array

--- tests/config-snapshots.spec.js
+++ tests/config-snapshots.spec.js
@@ -45,6 +45,18 @@ describe("generate config for esm projects", () => {
         }
     }
 
+    // add a combination that should produce an empty config
+    inputs.push({
+        name: "empty",
+        answers: {
+            purpose: "syntax",
+            moduleType: "esm",
+            framework: "none",
+            language: "javascript",
+            env: []
+        }
+    });
+
     inputs.forEach(item => {
         test(`${item.name}`, () => {
             const generator = new ConfigGenerator({ cwd: esmProjectDir, answers: item.answers });
