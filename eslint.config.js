c899b0c9085ff9907f7f6a22d48cc257d70da194
루밀LuMir <rpfos@naver.com>
2025-05-20 16:53:54 +0900

    chore: use `defineConfig` and `globalIgnores` in ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigESLint from "eslint-config-eslint";
 import eslintConfigESLintFormatting from "eslint-config-eslint/formatting";
 
-export default [
-    {
-        ignores: [
-            "coverage/",
-            "tests/fixtures/"
-        ]
-    },
-    ...eslintConfigESLint,
+export default defineConfig([
+    globalIgnores(["coverage/", "tests/fixtures/"]),
+    eslintConfigESLint,
     eslintConfigESLintFormatting
-];
+]);
