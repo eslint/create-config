c3120669eeb3ac4779c6ad1ba62538bf3acd7f06
hyperz111 <firzeroun11@gmail.com>
2025-06-28 22:45:12 +0700

    Update UI, Fix Generator, Add Dependencies, and Change Utilities

--- bin/create-config.js
+++ bin/create-config.js
@@ -7,20 +7,38 @@
 
 import { ConfigGenerator } from "../lib/config-generator.js";
 import { findPackageJson } from "../lib/utils/npm-utils.js";
-import { info } from "../lib/utils/logging.js";
+import { info, error, warn } from "../lib/utils/logging.js";
 import process from "node:process";
-import fs from "node:fs/promises";
+import semverSatisfies from 'semver/functions/satisfies.js';
+import colors from 'ansi-colors';
+import pkg from '../package.json' with {type:'json'};
 
-const pkg = JSON.parse(await fs.readFile(new URL("../package.json", import.meta.url), "utf8"));
+info((colors.blueBright(`\n${pkg.name}: v${pkg.version}\n`)));
 
-info(`${pkg.name}: v${pkg.version}\n`);
+Object.defineProperty(process, 'version', {value:'v11.0.4'})
+
+// Warn if your Node.js version is NOT compatible
+const myNodeVersion = process.version.substring(1);
+const compatibleVersionRange = pkg.engines.node;
+const compatibleVersionRangeText = compatibleVersionRange.replace(/(\s)*/g, '').replace(/\|\|/g, ', or ');
+if (!semverSatisfies(myNodeVersion, compatibleVersionRange)) {
+    warn(`Need Node.js version ${colors.cyan(compatibleVersionRangeText)}. But your Node.js version is ${colors.red(myNodeVersion)}. Please upgrade your Node.js first.`)
+    process.exit(0) // Exit gracefully
+}
 
 const cwd = process.cwd();
 const packageJsonPath = findPackageJson(cwd);
 
+// Showing if you close prompt
+const operationCancelled = () => {
+    error(`Operation cancelled.`);
+    process.exit(0); // Exit gracefully
+};
+
 if (packageJsonPath === null) {
-    throw new Error("A package.json file is necessary to initialize ESLint. Run `npm init` to create a package.json file and try again.");
-}
+    error(`A package.json file is necessary to initialize ESLint. Please run ${colors.dim('`npm init`')} to create a package.json file and try again.`);
+    process.exit(1);
+};
 
 const argv = process.argv;
 const sharedConfigIndex = process.argv.indexOf("--config");
@@ -28,9 +46,9 @@ const sharedConfigIndex = process.argv.indexOf("--config");
 if (sharedConfigIndex === -1) {
     const generator = new ConfigGenerator({ cwd, packageJsonPath });
 
-    await generator.prompt();
+    await generator.prompt().catch(() => operationCancelled());
     await generator.calc();
-    await generator.output();
+    await generator.output().catch(() => operationCancelled());
 } else {
 
     // passed "--config"
@@ -40,5 +58,5 @@ if (sharedConfigIndex === -1) {
     const generator = new ConfigGenerator({ cwd, packageJsonPath, answers });
 
     await generator.calc();
-    await generator.output();
+    await generator.output().catch(() => operationCancelled());
 }
