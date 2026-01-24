ae040ee24034c6f78d8504833f6b982896b4befa
루밀LuMir <rpfos@naver.com>
2026-01-24 22:59:39 +0900

    chore: introduce `prettier`

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigESLint from "eslint-config-eslint";
-import eslintConfigESLintFormatting from "eslint-config-eslint/formatting";
 
 export default defineConfig([
-    globalIgnores(["coverage/", "tests/fixtures/"]),
-    eslintConfigESLint,
-    eslintConfigESLintFormatting
+	globalIgnores(["coverage/", "tests/fixtures/"]),
+	eslintConfigESLint,
 ]);
